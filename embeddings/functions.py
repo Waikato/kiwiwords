f7b432d5a7d35f91d2ff7d3e82bd92fe41a90a16
felipebravom <felipebravom@gmail.com>
2018-12-17 16:26:37 +1300

    gold pairs

--- embeddings/functions.py
+++ embeddings/functions.py
@@ -8,13 +8,18 @@ Created on Thu Dec 13 15:08:36 2018
 
 import glob
 import pandas as pa
+from gensim.models.phrases import Phrases, Phraser
+from gensim.models.word2vec import LineSentence
+from gensim.models import Word2Vec
+from gensim.models import FastText
+
 from .twokenize import tokenizeRawTweetText
 
 
 def corpus2plainfile(source,target,extension='csv',col_name='text',sep="\t"):
         """
         converts a folder with csv files into a single file including just the tweet content. 
-        Tweet are tokenized with tweetNLP and lowercased.
+        Tweets are tokenized with tweetNLP and lowercased.
         One line per tweet.
         """    
        
@@ -31,3 +36,73 @@ def corpus2plainfile(source,target,extension='csv',col_name='text',sep="\t"):
 
 
 
+
+
+def proc_phrases(source,target):
+    """
+    runs a PMI-based phrase detector on a corpus of lines.
+    Sentences with detected phrases are stored in target.
+    """      
+
+    sentences = LineSentence(source)
+    
+     
+    common_terms = ["of", "with", "without", "and", "or", "the", "a"]
+    # Create the relevant phrases from the list of sentences:
+    phrases = Phrases(sentences, common_terms=common_terms)
+    # The Phraser object is used from now on to transform sentences
+    bigram = Phraser(phrases)
+    with open(target, 'a') as out_file:
+        for sentence in bigram[sentences]:
+            out_file.write(' '.join(sentence)+"\n")
+    
+    
+    
+def train_wordvectors(source,target):
+    emb_models=[Word2Vec,FastText]
+    window_sizes = [1,2,3,5,10,15]
+    vector_dims = [10,50,100,200,400,800]
+    min_c = 10
+    numWorkers = 20
+    
+    sentences = LineSentence(source)     
+     
+    for emb_mod in emb_models:
+        model = emb_mod(sentences, min_count=min_c, window = 5, size = 100)
+        model.save(target+"-"+model.__class__.__name__+'.bin')
+        
+       
+     
+ 
+ # =============================================================================
+ # 
+ # for  in windowSizes:
+ #     for embeddingS in embeddingSizes:
+ #         for corporaName, copora in coporaList:
+ #             for modelType in modelList:
+ #                 newModel = modelType(copora, size=embeddingS, window=windowS, min_count=minCount, workers=numWorkers)
+ #                 modelName = newModel.__class__.__name__+"-"+str(embeddingS)+"-"+str(windowS)+"-"+corporaName
+ #                 print(modelName)
+ #                 getRankAndOutput(newModel, goldPairs, modelName)
+ # =============================================================================
+ 
+ 
+ 
+ # train model
+# model = FastText(sentences, min_count=1, window = 5, size = 100)
+# # summarize the loaded model
+# print(model)
+# # summarize vocabulary
+# words = list(model.wv.vocab)
+# print(words)
+# # access vector for one word
+# print(model['sentence'])
+# # save model
+# model.save('model.bin')
+# # load model
+# new_model = Word2Vec.load('model.bin')
+# print(new_model)
+
+    
+    
+    
--- main.py
+++ main.py
@@ -5,90 +5,48 @@ Created on Fri Dec 14 11:44:49 2018
 
 @author: fbravoma
 """
-from gensim.models import Word2Vec
-from gensim.models import FastText
-from gensim.test.utils import datapath
-from gensim.models.phrases import Phrases, Phraser
-from gensim.models.word2vec import LineSentence
-from embeddings.functions import corpus2plainfile
+
+from embeddings.functions import corpus2plainfile, proc_phrases, train_wordvectors
 import argparse
 
 
 parser = argparse.ArgumentParser(description='Process Maori Loandwords tweets and train embeddings')
+
+
+
+parser.add_argument('--proc_corpus', action='store_true',
+                    help='Process corpus')
+
 
 parser.add_argument('--corpus_path', type=str, default="data",
                     help='Path to the corpus folder',
                     metavar='')
 
 
-parser.add_argument('--master_file', type=str, default="masterTweets.txt",
+parser.add_argument('--tweet_outfile', type=str, default="all_tweets.txt",
                     help='Name of processed corpus',
                     metavar='')
 
 
-parser.add_argument('--process_corpus', action='store_true',
-                    help='Process corpus')
+parser.add_argument('--proc_phrases', action='store_true',
+                    help='Extract phrases')
 
+parser.add_argument('--phrase_outfile', type=str, default="all_tweets_phrases.txt",
+                    help='Name of processed corpus after detecting phrases',
+                    metavar='')
+
+
+parser.add_argument('--train_wordvectors', action='store_true',
+                    help='Train word vectors')
 
 args = parser.parse_args()
 
-if args.process_corpus:
-    corpus2plainfile(args.corpus_path,args.master_file)     
+if args.proc_corpus:
+    corpus2plainfile(args.corpus_path,args.tweet_outfile)   
+    
+    
+if args.proc_phrases:
+     proc_phrases(args.tweet_outfile,args.phrase_outfile)
 
-# =============================================================================
-# 
-# 
-# sentences = LineSentence("../embeddings/masterTweets.txt")
-# 
-# common_terms = ["of", "with", "without", "and", "or", "the", "a"]
-# # Create the relevant phrases from the list of sentences:
-# phrases = Phrases(sentences, common_terms=common_terms)
-# # The Phraser object is used from now on to transform sentences
-# bigram = Phraser(phrases)
-# # Applying the Phraser to transform our sentences is simply
-# all_sentences = list(bigram[sentences])
-# 
-# 
-# emb_models=[Word2Vec,FastText]
-# window_sizes = [1,2,3,5,10,15]
-# vector_dims = [10,50,100,200,400,800]
-# min_c = 10
-# numWorkers = 20
-# 
-# 
-# for emb_mod in emb_models:
-#     model = emb_mod(sentences, min_count=min_c, window = 5, size = 100)
-#     print(model)
-#     words = list(model.wv.vocab)
-#     print(words)
-#     
-# 
-# # =============================================================================
-# # 
-# # for  in windowSizes:
-# #     for embeddingS in embeddingSizes:
-# #         for corporaName, copora in coporaList:
-# #             for modelType in modelList:
-# #                 newModel = modelType(copora, size=embeddingS, window=windowS, min_count=minCount, workers=numWorkers)
-# #                 modelName = newModel.__class__.__name__+"-"+str(embeddingS)+"-"+str(windowS)+"-"+corporaName
-# #                 print(modelName)
-# #                 getRankAndOutput(newModel, goldPairs, modelName)
-# # =============================================================================
-# 
-# 
-# 
-# # train model
-# model = FastText(sentences, min_count=1, window = 5, size = 100)
-# # summarize the loaded model
-# print(model)
-# # summarize vocabulary
-# words = list(model.wv.vocab)
-# print(words)
-# # access vector for one word
-# print(model['sentence'])
-# # save model
-# model.save('model.bin')
-# # load model
-# new_model = Word2Vec.load('model.bin')
-# print(new_model)
-# =============================================================================
+if args.train_wordvectors:
+    train_wordvectors('all_tweets_phrases.txt','phrases')
