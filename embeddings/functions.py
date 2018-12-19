8678c6e45e52aaa24aaaece411e6d83f01bb14bf
felipebravom <felipebravom@gmail.com>
2018-12-19 16:27:06 +1300

    new files

--- embeddings/functions.py
+++ embeddings/functions.py
@@ -12,8 +12,17 @@ from gensim.models.phrases import Phrases, Phraser
 from gensim.models.word2vec import LineSentence
 from gensim.models import Word2Vec
 from gensim.models import FastText
+from statistics import median
 
 from .twokenize import tokenizeRawTweetText
+
+
+
+import numpy
+import os
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
 
 
 def corpus2plainfile(source,target,extension='csv',col_name='text',sep="\t"):
@@ -59,50 +68,92 @@ def proc_phrases(source,target):
     
     
 def train_wordvectors(source,target):
+    """
+    Trains word vectors using various parameters
+    """      
     emb_models=[Word2Vec,FastText]
     window_sizes = [1,2,3,5,10,15]
     vector_dims = [10,50,100,200,400,800]
     min_c = 10
-    numWorkers = 20
+#    numWorkers = 20
     
     sentences = LineSentence(source)     
      
-    for emb_mod in emb_models:
-        model = emb_mod(sentences, min_count=min_c, window = 5, size = 100)
-        model.save(target+"-"+model.__class__.__name__+'.bin')
+    for w in window_sizes:
+        for v_dim in vector_dims:
+            for emb_mod in emb_models:
+                model = emb_mod(sentences, min_count=min_c, window = w, size = v_dim)
+                model.save(target+"/"+model.__class__.__name__+'-w'+str(w)+"-d"+str(v_dim)+'.bin')
         
        
      
- 
- # =============================================================================
- # 
- # for  in windowSizes:
- #     for embeddingS in embeddingSizes:
- #         for corporaName, copora in coporaList:
- #             for modelType in modelList:
- #                 newModel = modelType(copora, size=embeddingS, window=windowS, min_count=minCount, workers=numWorkers)
- #                 modelName = newModel.__class__.__name__+"-"+str(embeddingS)+"-"+str(windowS)+"-"+corporaName
- #                 print(modelName)
- #                 getRankAndOutput(newModel, goldPairs, modelName)
- # =============================================================================
- 
- 
- 
- # train model
-# model = FastText(sentences, min_count=1, window = 5, size = 100)
-# # summarize the loaded model
-# print(model)
-# # summarize vocabulary
-# words = list(model.wv.vocab)
-# print(words)
-# # access vector for one word
-# print(model['sentence'])
-# # save model
-# model.save('model.bin')
-# # load model
-# new_model = Word2Vec.load('model.bin')
-# print(new_model)
 
+
+def eval_embeddings(model_folder, gold_pairs, target):
+    """
+    Evaluates word embeddings
+    """ 
+    pairs = pa.read_csv(gold_pairs,sep='\t')
+    
+    model_files = [i for i in glob.glob(model_folder+'/*.{}'.format("bin"))]
+    with open(target, 'a') as out_file:
+        out_file.write("model\tmeadian.rank\n")
+    
+        for model_file in model_files:    
+            model = Word2Vec.load(model_file)
+            ranks = []
+            for index, row in pairs.iterrows():
+                eng,mao = row['English'], row['Te Reo Maori']
+                if eng in model.wv.vocab and mao in model.wv.vocab:
+                    ranks.append(model.wv.rank(eng,mao))
+                
+            out_file.write(model_file+"\t"+str(median(ranks))+"\n")
+    
+  
+    
+
+
+ 
+def t_sne_scatterplot(model_file = "experiments/Word2Vec-w5-d400.bin", words = ['marae','kia_ora','whanau','wahine','hapu','kura','haka'], target = "pics"):
+    """
+    plots word vectors
+    """ 
+    
+    model = Word2Vec.load(model_file)
+    
+    for word in words:   
+        vocab = list(model.wv.vocab.keys())
+        dim0 = model.wv[vocab[0]].shape[0]
+        arr = numpy.empty((0, dim0), dtype='f')
+        w_labels = []
+        
+        w_labels.append(word)
+        nearby = model.wv.similar_by_word(word, topn=40)
+        arr = numpy.append(arr, numpy.array([model[word]]), axis=0)
+        for n in nearby:
+            w_vec = model[n[0]]
+            w_labels.append(n[0])
+            arr = numpy.append(arr, numpy.array([w_vec]), axis=0)
+     
+        tsne = TSNE(n_components=2, random_state=1)
+        numpy.set_printoptions(suppress=True)
+        Y = tsne.fit_transform(arr)
+        x_coords = Y[:, 0]
+        y_coords = Y[:, 1]
+         
+        plt.rc("font", size=9)
+        plt.figure(figsize=(16, 12), dpi=80)
+        plt.scatter(x_coords[0], y_coords[0], s=800, marker="o", color="blue")
+        plt.scatter(x_coords[1:], y_coords[1:], s=200, marker="o", color="red")
+     
+        for label, x, y in zip(w_labels, x_coords, y_coords):
+            plt.annotate(label.upper(), xy=(x, y), xytext=(0, 0), textcoords='offset points')
+      
+        plt.xlim(x_coords.min()-50, x_coords.max()+50)
+        plt.ylim(y_coords.min()-50, y_coords.max()+50)
+        filename = os.path.join(target+"/"+word+"_tsne.png")
+        plt.savefig(filename)
+        plt.close()
     
     
     
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Created on Fri Dec 14 11:44:49 2018
 @author: fbravoma
 """
 
-from embeddings.functions import corpus2plainfile, proc_phrases, train_wordvectors
+from embeddings.functions import corpus2plainfile, proc_phrases, train_wordvectors, eval_embeddings, t_sne_scatterplot
 import argparse
 
 
@@ -18,7 +18,7 @@ parser.add_argument('--proc_corpus', action='store_true',
                     help='Process corpus')
 
 
-parser.add_argument('--corpus_path', type=str, default="data",
+parser.add_argument('--corpus_path', type=str, default="data/rawtweets",
                     help='Path to the corpus folder',
                     metavar='')
 
@@ -39,6 +39,18 @@ parser.add_argument('--phrase_outfile', type=str, default="all_tweets_phrases.tx
 parser.add_argument('--train_wordvectors', action='store_true',
                     help='Train word vectors')
 
+
+
+parser.add_argument('--eval_wordvectors', action='store_true',
+                    help='Evaluate word vectors')
+
+
+
+
+parser.add_argument('--plot_wordvectors', action='store_true',
+                    help='Plot word vectors')
+
+
 args = parser.parse_args()
 
 if args.proc_corpus:
@@ -49,4 +61,11 @@ if args.proc_phrases:
      proc_phrases(args.tweet_outfile,args.phrase_outfile)
 
 if args.train_wordvectors:
-    train_wordvectors('all_tweets_phrases.txt','phrases')
+    train_wordvectors('all_tweets_phrases.txt','experiments')
+    
+if args.eval_wordvectors:
+    eval_embeddings('experiments','data/gold_pairs.csv','emb_res.csv')
+    
+if args.plot_wordvectors:
+    t_sne_scatterplot()
+    
